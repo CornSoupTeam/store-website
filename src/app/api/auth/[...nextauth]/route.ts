d89260ea98fe06e3e49a9803c0477454b011b123
Jihyuk Lee <wh3n_gg@kakao.com>
2024-03-18 03:56:12 +0000

    🧰 :: 내보내기 삭제

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import DiscordProvider from "next-auth/providers/discord";
 import { NextAuthOptions } from "next-auth";
 
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 
   providers: [
